dafae97b2614b2b5266d10cae315884c04532b8b
enginoobz <enginoobz@gmail.com>
2022-03-08 04:52:14 +0100

    Minor refactoring

--- Assets/_Project/THOR_ThunderstormExtension.cs
+++ Assets/_Project/THOR_ThunderstormExtension.cs
@@ -13,20 +13,21 @@ using Enginooby.Attribute;
 /// </summary>
 [RequireComponent(typeof(THOR_Thunderstorm))]
 public class THOR_ThunderstormExtension : MonoBehaviour {
-  // TODO: Randomize
+  // TODO: Randomize offsets
+  [SerializeField] private KeyCode _spawnAtPlayerKey = KeyCode.L;
   [SerializeField] private Vector3 _offsetToPlayer = new(0, 100, 0);
-  [SerializeField] private KeyCode _spawnNearPlayerKey = KeyCode.L;
   [SerializeField] private bool _enableSpawnAtMouseClick = true;
   [SerializeField] private Vector3 _offsetToMouse = new(0, 100, 0);
   
   private GameObject _player;
+  private THOR_Thunderstorm Thunderstorm => THOR_Thunderstorm.instance;
 
   private void Start() {
     _player = GameObject.FindGameObjectWithTag("Player");
   }
 
   private void Update() {
-   if(_spawnNearPlayerKey.IsDown()) SpawnLightingNearPlayer();
+   if(_spawnAtPlayerKey.IsDown()) SpawnLightingAtPlayer();
    if(_enableSpawnAtMouseClick) SpawnLightingAtMouseClick();
   }
 
@@ -37,15 +38,15 @@ public class THOR_ThunderstormExtension : MonoBehaviour {
   }
   
   public void SpawnLighting(Vector3 pos) {
-    var lastPos = THOR_Thunderstorm.instance.spawnPos;
-    THOR_Thunderstorm.instance.spawnPos = pos;
-    THOR_Thunderstorm.instance.ActivateLightning();
-    THOR_Thunderstorm.instance.spawnPos = lastPos;
+    var lastPos = Thunderstorm.spawnPos;
+    Thunderstorm.spawnPos = pos;
+    Thunderstorm.ActivateLightning();
+    Thunderstorm.spawnPos = lastPos;
   }
 
   [Command("s.lighting")]
   [Enginooby.Attribute.Button]
-  private void SpawnLightingNearPlayer() {
+  private void SpawnLightingAtPlayer() {
     SpawnLighting(_player.transform.position + _offsetToPlayer);
   }
 }
